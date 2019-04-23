c00736efde8c3a139f91939e5211e3f6fb7abe4b
starchevskayamaria17 <m.starchevskaia@g.nsu.ru>
2019-04-23 14:54:51 +0700

    my stack

--- paranthesis_balanced.py
+++ paranthesis_balanced.py
@@ -1,6 +1,31 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def is_paranthesis_balanced(s):
-    pass
 
+from stack import Stack
+
+def is_paranthesis_balanced(s):
+    openBracket = ["[","{","(", "<"]
+    closeBracket = ["]","}",")", ">"]
+    stack = Stack(len(s))
+    for i in range(len(s)):
+        if s[i] in openBracket:
+            stack.push(s[i])
+        elif s[i] in closeBracket:
+            if stack.__len__() == 0:
+                return False
+            else:
+                del_bracket = stack.pop()
+                if del_bracket == '(' and s[i] == ')':
+                    continue
+                elif del_bracket == '[' and s[i] == ']':
+                    continue
+                elif del_bracket == '{' and s[i] == '}':
+                    continue
+                elif del_bracket == '<' and s[i] == '>':
+                    continue
+                else:
+                    return False 
+    if stack.__len__() == 0:
+        return True
+            
--- stack.py
+++ stack.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 class StackOverflowError(RuntimeError):
     pass
 
@@ -14,12 +13,21 @@ class Stack:
     def __init__(self, size):
         self.storage = [0] * size
         self.head = -1
+        self.size = size
 
     def push(self, v):
-        pass
+        if self.head >= self.size - 1: 
+            raise StackOverflowError(RuntimeError)        
+        self.head += 1
+        self.storage[self.head] = v
+        return self.storage
 
     def pop(self):
-        pass
+        if self.head == -1:
+            raise StackIsEmptyError(RuntimeError)
+        d = self.storage[self.head]
+        self.head -= 1
+        return d
 
     def __len__(self):
-        pass
+        return self.head + 1
